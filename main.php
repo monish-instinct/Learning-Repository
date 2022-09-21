722cc4b3b68d92b180a4fa28889d54b85926ca7f
InstincT <113701884+Instinctcms@users.noreply.github.com>
2022-09-21 17:29:36 +0530

    Add files via upload

---
+++ main.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+        <link rel="stylesheet" type="text/css" href="main.css"  />
+      <title>Login  - InstincT</title>
+      <body style="background: #262626">
+      <a class = "a" href="registerform.php">INSTINCT</a><br><br><br><br><br>
+      <hr>
+      <div class="wrap">
+       <div class="center">
+                <button class="button" name= "submit">Online Registration</button><br><br><br><br><br>
+                <button class="button1" name= "submit">Student Region</button><br><br><br><br><br>
+                <button class="button2" name= "submit">Teacher Sector</button>
+</div>
+</div>
+    </body>
+    </html>
+
